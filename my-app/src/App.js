f0e2e0007100b80dcdcb8b750c1d9bb2006c492e
jessjat <mutanujat@gmail.com>
2025-06-23 13:13:56 +0300

    Initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import dictionary from"./dictionary.png";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+    <img src={dictionary}className="App-pic img-fluid" alt="pic" />
+   
+      </header>
+    
+    </div>
+  );
+}
+
+export default App;
